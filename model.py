27abd7cad908f49272380de70fd23651e044c3da
AdrianKnapczyk <adrian.knapczyk91@gmail.com>
2019-11-14 21:00:41 +0100

    Dodawanie licznika

--- model.py
+++ model.py
@@ -1,34 +1,40 @@
 import random
 
+
 def initialize_board():
     _board = {}
     for x in range(20):
         for y in range(20):
-            _board[(x,y)] = None
+            _board[(x, y)] = None
     return _board
 
+
 def initialize_snake(_board):
-    _snake = [(random.randint(8,12), random.randint(8,12))]
+    _snake = [(random.randint(8, 12), random.randint(8, 12))]
     _board[_snake[0]] = "SnakeHead"
     return _snake
 
+
 def game_over(board, coordinate):
-    if coordinate[0] < 0 or coordinate[0] > 19 or coordinate[1] < 0 or coordinate[1] > 19 or board[coordinate] == "SnakeHead":
+    if coordinate[0] < 0 or coordinate[0] > 19 or coordinate[1] < 0 or coordinate[1] > 19 or board[
+        coordinate] == "SnakeHead":
         exit(2)
+
 
 def move_tail(snake):
     return snake[:-1]
+
 
 def set_new_position(direction, snake, board):
     move_tail(snake)
     head_x, head_y = snake[0]
     if direction == 0:
         head_y = head_y - 1
-    if direction == 1:
+    elif direction == 1:
         head_x = head_x + 1
-    if direction == 2:
+    elif direction == 2:
         head_y = head_y + 1
-    if direction == 3:
+    elif direction == 3:
         head_x = head_x - 1
     game_over(board, coordinate=(head_x, head_y))
     board[(head_x, head_y)] = "SnakeHead"
@@ -39,16 +45,21 @@ def set_new_position(direction, snake, board):
         board[elem] = "SnakeHead"
     return snake
 
+
 def initialize_apple(_board):
-    _apple = (random.randint(0,19),random.randint(0,19))
+    _apple = (random.randint(0, 19), random.randint(0, 19))
     while _board[_apple] is not None:
         _apple = (random.randint(0, 19), random.randint(0, 19))
     _board[_apple] = "Apple"
     return _apple
 
-def eat_apple(board,snake, apple):
+
+def eat_apple(board, snake, apple):
     if snake[0] == apple:
         snake.append(apple)
         print(snake)
         return initialize_apple(board)
     return apple
+
+def get_score(snake):
+    return len(snake)
--- view.py
+++ view.py
@@ -1,13 +1,13 @@
 import pygame
 
 
-def draw(board, screen):
+def draw(board, screen,score):
     for coordinates, value in board.items():
         if value == "SnakeHead":
             head_x = coordinates[0] * 20
             head_y = coordinates[1] * 20
-            head_rect = pygame.Rect(head_x,head_y,20,20)
-            pygame.draw.rect(screen, (128,128,128), head_rect)
+            head_rect = pygame.Rect(head_x, head_y, 20, 20)
+            pygame.draw.rect(screen, (128, 128, 128), head_rect)
         elif value is None:
             head_x = coordinates[0] * 20
             head_y = coordinates[1] * 20
@@ -18,5 +18,6 @@ def draw(board, screen):
             head_y = coordinates[1] * 20
             head_rect = pygame.Rect(head_x, head_y, 20, 20)
             pygame.draw.rect(screen, (255, 0, 0), head_rect)
-
-
+    font = pygame.font.Font(pygame.font.get_default_font(),20)
+    text = font.render(str(score),True,(255,255,0))
+    screen.blit(text,dest=(20,20))
